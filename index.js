8dd7691dc95bbbc2b02240d27b1938e2e71df94a
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2026-01-02 22:08:26 +0530

    project created

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/todos", require("./routes/todo"));
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Todo API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
